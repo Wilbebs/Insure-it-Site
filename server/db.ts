1f08aaa73160ccc9334f1a8b6f20352b812e25df
hernwilbwork <47312896-hernwilbwork@users.noreply.replit.com>
2026-03-02 00:33:33 +0000

    Restored to '5d0815c0a3ae7a281d2734096d69f13028530a52'
    Replit-Restored-To: 5d0815c0a3ae7a281d2734096d69f13028530a52

--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,7 @@
 import { Pool } from 'pg';
 
-const connectionString = process.env.RDS_DATABASE_URL || process.env.DATABASE_URL;
-
 export const pool = new Pool({
-  host: process.env.RDS_HOST || undefined,
-  port: process.env.RDS_PORT ? parseInt(process.env.RDS_PORT) : undefined,
-  user: process.env.RDS_USER || undefined,
-  password: process.env.RDS_PASSWORD || undefined,
-  database: process.env.RDS_DBNAME || undefined,
-  connectionString: (process.env.RDS_HOST ? undefined : connectionString),
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }

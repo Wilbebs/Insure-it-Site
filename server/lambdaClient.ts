5fcaf38d08ca55bb8ef9b7afba8ab4353c6945ba
Wilbert Hernandez <92603159+Wilbebs@users.noreply.github.com>
2026-04-20 22:42:23 +0000

    fix: use server iam role

--- server/lambdaClient.ts
+++ server/lambdaClient.ts
@@ -1,12 +1,6 @@
 import { LambdaClient } from "@aws-sdk/client-lambda";
 
-export const lambdaClient = new LambdaClient({
-  region: process.env.AWS_REGION || "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
+export const lambdaClient = new LambdaClient({ region: "us-east-1" });
 
 export const LAMBDA_NOTIFICATION_FUNCTION =
   process.env.LAMBDA_NOTIFICATION_FUNCTION || "sendFormNotification";
